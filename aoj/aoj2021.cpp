fc7c4631499b5d349cf2f0d47a81b6ecba3099d7
ryokamoi <ryokamoi.jp@gmail.com>
2019-02-12 09:38:44 -0500

    aoj2021

--- aoj/aoj2021.cpp
+++ aoj/aoj2021.cpp
@@ -1,96 +1,104 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 #define REP(i, n) for(int i=0; i<n; i++)
+#define INF 1000000000
 
-int INF = 10e8;
-int d[100][100];
-int n, m, l, k, a, h;
-bool cold[100];
+typedef pair<int, int> Pii;
+typedef pair<int, Pii> P; // time, town, coldtime
 
-int main(){
-    while(1){
+struct edge {int to, cost; };
+
+int coldtown[110];
+int d[110][110];
+
+int main() {
+    while(true) {
+        int n, m, l, k, a, h;
         cin >> n >> m >> l >> k >> a >> h;
-        
-        if(n==0) break;
+        if (n == 0) break;
 
-        REP(i, n){
-            REP(j, n){
-                if(i==j){
-                    d[i][j] = 0;
-                } else {
-                    d[i][j] = INF;
-                }
+        vector<edge> g[110];
+        // initialization
+        REP(i, n) {
+            REP(j, m+1) {
+                d[i][j] = INF;
             }
         }
+        d[h][0] = 0;
 
-        REP(i, n) cold[i] = false;
-        REP(i, l){
-            int cold_ind;
-            cin >> cold_ind;
-            cold[cold_ind] = true;
+        // cold town
+        REP(i, n) coldtown[i] = 0;
+        REP(i, l) {
+            int idx;
+            cin >> idx;
+            coldtown[idx] = 1;
         }
-        cold[a] = true;
-        cold[h] = true;
 
-        REP(i, k){
+        REP(i, k) {
             int x, y, t;
             cin >> x >> y >> t;
-            d[x][y] = t;
-            d[y][x] = t;
+
+            edge e1;
+            e1.to = y;
+            e1.cost = t;
+            g[x].push_back(e1);
+
+            edge e2;
+            e2.to = x;
+            e2.cost = t;
+            g[y].push_back(e2);
         }
 
-        REP(i, n){
-            if(!cold[i]){
-               REP(j, n){
-                   REP(k, n){
-                       d[j][k] = min(d[j][k], d[j][i] + d[i][k]);
-                       d[k][j] = d[j][k];
-                   }
-               }
-            }
-        }
-        REP(i, n){
-            if(!cold[i]){
-                REP(j, n){
-                    d[i][j] = INF;
-                    d[j][i] = INF;
+        // dijkstra
+        priority_queue<P> que;
+        P p = P(0, Pii(h, 0));
+        que.push(p);
+
+        while(!que.empty()) {
+            P p = que.top(); que.pop();
+            int time = p.first;
+            int town = p.second.first;
+            int ctime = p.second.second;
+            if (d[town][ctime] < time) continue;
+
+            REP(i, g[town].size()) {
+                int to = g[town][i].to;
+                int cost = g[town][i].cost;
+
+                if (m < ctime + cost) continue; // cannot achieve
+                
+                int ntime, nctime;
+                if (coldtown[to]) {
+                    ntime = time + cost + (cost + ctime);
+                    nctime = 0;
+                } else {
+                    ntime = time + cost;
+                    nctime = ctime + cost;
+                }
+                if (d[to][nctime] > ntime) {
+                    d[to][nctime] = ntime;
+                    que.push(P(ntime, Pii(to, nctime)));
                 }
             }
         }
 
-        /*
-        REP(i, n){
-            REP(j, n){
-                cout << d[i][j] << " ";
-            }
-            cout << endl;
-        }
-        */
-
-        REP(i, n){
-            REP(j, n){
-                if(d[i][j] > m){
-                    d[i][j] = INF;
-                }
+        int output = INF;
+        REP(i, m+1) {
+            if (d[a][i] != INF) {
+                int ctime = (d[a][i] - i) / 2;
+                output = min(d[a][i] - min(ctime, m-i), output);
             }
         }
 
-        REP(k, n){
-            REP(i, n){
-                REP(j, n){
-                    d[i][j] = min(d[i][j], d[i][k]+d[k][j]);
-                }
-            }
-        }
-
-        if(d[a][h] == INF){
+        if (output == INF) {
             cout << "Help!" << endl;
         } else {
-            int output = d[a][h] + max(0, d[a][h] - m);
             cout << output << endl;
         }
     }
 }
+
