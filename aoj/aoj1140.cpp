baa9f0151277db1dbf917ee090a2d857f5460ad9
ryokamoi <ryokamoi.jp@gmail.com>
2019-05-05 10:55:20 -0400

    aoj1140

--- aoj/aoj1140.cpp
+++ aoj/aoj1140.cpp
@@ -1,67 +1,157 @@
+// Ryo Kamoi
+// #define DEBUG
+
 #include<iostream>
-#include<queue>
-#include<map>
-#include<string>
+#include<math.h>
 #include<vector>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<cstring>
 
 using namespace std;
 
-#define REP(i, n) for(int i=0; i++; i<n)
+#define REP(i, n) for(int i=0; i<n; i++)
 
 typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+typedef pair<int, pii> piii;
+
+int INF = 1000000000;
 
 int w, h;
-int dnum;
-char tiles[30][30];
-int tiledist[30][30];
 int dist[20][20];
+int tile[30][30];
+int tile_dist[30][30];
+vector<pii> points;
+int dirty_num;
 
 int x[4] = {0, 0, -1, 1};
 int y[4] = {-1, 1, 0, 0};
 
-vector<pii> dtiles;
-map<pii, int> dtiles_idx;
+int dp[1 << 12][12];
 
-void cleandist() {
+void init_dist() {
     REP(i, h) {
         REP(j, w) {
-            tiledist[i][j] = 0;
+            tile_dist[i][j] = INF;
         }
     }
 }
 
-void calcdist(int idx) {
-    
+void calc_dist(int idx) {
+    init_dist();
+    queue<piii> que;
+    que.push(piii(0, points[idx]));
+    tile_dist[points[idx].first][points[idx].second] = 0;
+
+    while (!que.empty()) {
+        piii p = que.front(); que.pop();
+        int d = p.first;
+        int ih = p.second.first;
+        int jw = p.second.second;
+        if (tile_dist[ih][jw] != d) continue;
+        
+        REP(i, 4) {
+            int ni = ih + y[i];
+            int nj = jw + x[i];
+
+            if (ni<0 || ni>=h || nj<0 || nj>=w) continue;
+            if (tile[ni][nj] == -1) continue;
+            if (tile_dist[ni][nj] <= d + 1) continue;
+            tile_dist[ni][nj] = d+1;
+            que.push(piii(d + 1, pii(ni, nj)));
+        }
+    }
+
+    REP(i, dirty_num+1) {
+        dist[idx][i] = tile_dist[points[i].first][points[i].second];
+    }
 }
 
-int main() {
+// S: history, v: current state
+int tsp(int S, int v) {
+    if (dp[S][v] >= 0) {
+        return dp[S][v];
+    }
+
+    if (S==(1<<(dirty_num+1))-1) {
+        return dp[S][v] = 0;
+    }
+
+    int res = INF;
+    REP(u, dirty_num+1) {
+        if (!(S>>u & 1)) {
+            res = min(res, tsp(S|1<<u, u) + dist[v][u]);
+        }
+    }
+    return dp[S][v] = res;
+}
+
+int main(){
     while(1) {
         cin >> w >> h;
-
         if (w==0) break;
 
-        dtiles = vector<pii>();
-        dtiles_idx = map<pii, int>();
-
-        dnum = 0;
         REP(i, h) {
-            string s;
-            cin >> s;
             REP(j, w) {
-                tiles[i][j] = s[j];
-                if (tiles[i][j] == 'o') {
-                    dtiles.insert(dtiles.begin(), pii(i, j));
+                tile[i][j] = 0;
+            }
+        }
+
+        points = vector<pii>();
+        dirty_num = 0;
+        REP(i, h) {
+            string row;
+            cin >> row;
+            REP(j, w) {
+                if (row[j] == 'o') {
+                    points.insert(points.begin(), pii(i, j));
                 }
-                if (tiles[i][j] == '*') {
-                    dtiles.push_back(pii(i, j));
-                    dnum++;
+                if (row[j] == '*') {
+                    dirty_num ++;
+                    points.push_back(pii(i, j));
+                }
+                if (row[j] == 'x') {
+                    tile[i][j] = -1;
                 }
             }
         }
 
-        REP(i, dtiles.size()) {
-            cleandist();
-            calcdist(i);
+        REP(i, dirty_num+1) {
+            REP(j, dirty_num+1) {
+                dist[i][j] = INF;
+            }
         }
+
+        REP(i, dirty_num+1) {
+            calc_dist(i);
+        }
+
+#ifdef DEBUG
+        REP(i, dirty_num+1) {
+            REP(j, dirty_num+1) {
+                cout << dist[i][j] << " ";
+            }
+            cout << endl;
+        }
+#endif
+
+        bool end = false;
+        REP(i, dirty_num+1) {
+            if (dist[0][i] == INF) {
+                cout << -1 << endl;
+                end = true;
+                break;
+            }
+        }
+        if (end) continue;
+
+        // TSP
+        memset(dp, -1, sizeof(dp));
+        cout << tsp(1, 0) << endl;
     }
 }
